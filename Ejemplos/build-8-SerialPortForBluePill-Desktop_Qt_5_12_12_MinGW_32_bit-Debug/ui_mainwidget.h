bb5ded05f39e439f7ba3b49a0b0e21a44fcc0fea
DaniJ2032 <dajjuarez@gmail.com>
2023-10-12 12:48:59 -0300

    Nuevo

---
+++ Ejemplos/build-8-SerialPortForBluePill-Desktop_Qt_5_12_12_MinGW_32_bit-Debug/ui_mainwidget.h
@@ -0,0 +1,75 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwidget.ui'
+**
+** Created by: Qt User Interface Compiler version 5.12.12
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWIDGET_H
+#define UI_MAINWIDGET_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QPlainTextEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_mainWidget
+{
+public:
+    QWidget *layoutWidget;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *Iniciar;
+    QPushButton *Finalizar;
+    QPlainTextEdit *plainTextEdit;
+
+    void setupUi(QWidget *mainWidget)
+    {
+        if (mainWidget->objectName().isEmpty())
+            mainWidget->setObjectName(QString::fromUtf8("mainWidget"));
+        mainWidget->resize(1095, 600);
+        layoutWidget = new QWidget(mainWidget);
+        layoutWidget->setObjectName(QString::fromUtf8("layoutWidget"));
+        layoutWidget->setGeometry(QRect(10, 450, 1071, 91));
+        horizontalLayout = new QHBoxLayout(layoutWidget);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        Iniciar = new QPushButton(layoutWidget);
+        Iniciar->setObjectName(QString::fromUtf8("Iniciar"));
+
+        horizontalLayout->addWidget(Iniciar);
+
+        Finalizar = new QPushButton(layoutWidget);
+        Finalizar->setObjectName(QString::fromUtf8("Finalizar"));
+
+        horizontalLayout->addWidget(Finalizar);
+
+        plainTextEdit = new QPlainTextEdit(mainWidget);
+        plainTextEdit->setObjectName(QString::fromUtf8("plainTextEdit"));
+        plainTextEdit->setGeometry(QRect(10, 10, 1071, 441));
+
+        retranslateUi(mainWidget);
+
+        QMetaObject::connectSlotsByName(mainWidget);
+    } // setupUi
+
+    void retranslateUi(QWidget *mainWidget)
+    {
+        mainWidget->setWindowTitle(QApplication::translate("mainWidget", "mainWidget", nullptr));
+        Iniciar->setText(QApplication::translate("mainWidget", "INICIAR", nullptr));
+        Finalizar->setText(QApplication::translate("mainWidget", "FINALIZAR", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class mainWidget: public Ui_mainWidget {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWIDGET_H
